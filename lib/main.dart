459c0ab4c19ba0cd8837582119fe6700c6e40e87
Ege <egeyarenci@gmail.com>
2020-12-20 19:45:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,41 +24,36 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: Theme.of(context).accentColor),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-               AssetImage("assets/images/icons/Home.png"),
-               ),
-              label: 'Home',
+      appBar: AppBar(backgroundColor: Theme.of(context).accentColor),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/Home.png"),
             ),
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-               AssetImage("assets/images/icons/Task.png"),
-               ),
-              label: 'Task',
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/Task.png"),
             ),
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-               AssetImage("assets/images/icons/Store.png"),
-               ),
-              label: 'Store',
+            label: 'Task',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/Store.png"),
             ),
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-               AssetImage("assets/images/icons/profile.png"),
-               ),
-              label: 'Profile',
+            label: 'Store',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/profile.png"),
             ),
-           
-          ],
-        ),
-        );
-
-
-
-        
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
