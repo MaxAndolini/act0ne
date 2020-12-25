ba3715ab9591f13325d5f05bece16328dd929514
Ege <egeyarenci@gmail.com>
2020-12-25 16:18:00 +0300

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Profile(),
-    ));
+    );
+  }
+}
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Theme.of(context).accentColor),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/Home.png"),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/Task.png"),
+            ),
+            label: 'Task',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/Store.png"),
+            ),
+            label: 'Store',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/images/icons/profile.png"),
+            ),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      // add profile buttons
+    );
   }
 }
