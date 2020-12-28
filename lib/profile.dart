4584f4f8c5131ee5d50b54729b313b5702e44267
Ege <egeyarenci@gmail.com>
2020-12-28 10:08:11 +0300

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -24,20 +24,49 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
-                radius: 85,
+                radius: 100,
                 child: Icon(
                   Icons.people_alt_rounded,
                 ),
               ),
+              SizedBox(height: 30),
+              Container(
+                margin: EdgeInsets.all(15.0),
+                padding: EdgeInsets.all(15.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Row(
+                  children: [
+                    Text(
+                      "SETTINGS",
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.all(15.0),
+                padding: EdgeInsets.all(15.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Row(
+                  children: [
+                    Text(
+                      "ABOUT US",
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
-        ],
+        ),
       ),
       appBar: AppBar(backgroundColor: Theme.of(context).accentColor),
       bottomNavigationBar: BottomNavigationBar(
