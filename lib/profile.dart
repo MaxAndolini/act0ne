7fd4ed0b3e2d369e0a7857782b4f77735a105af9
Mustafa ALAN <mustafa.alann@hotmail.com>
2021-01-08 16:58:03 +0300

    get username from firestorage(working on)

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,6 @@
 import 'package:act0ne/authentication_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,8 +11,24 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String username;
+  String surname;
+
+  void getData() async {
+    var docRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc('C91Z6pmKD0hE1MQTSkIu');
+    var docSnap = await docRef.get();
+    var docData = docSnap.data();
+    username = docData['name'];
+    surname = docData['surname'];
+    print(username);
+  }
+
+  @override
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Container(
         child: Padding(
@@ -40,6 +58,11 @@ class _ProfileState extends State<Profile> {
                       }
                       return Container();
                     }),
+              ),
+              SizedBox(height: 30),
+              Text(
+                username + " " + surname,
+                style: TextStyle(fontSize: 30),
               ),
               SizedBox(height: 30),
               InkWell(
