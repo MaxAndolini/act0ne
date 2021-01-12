592df2f650481fe245e7880ff6d924b7f73cfa6d
Mustafa ALAN <mustafa.alann@hotmail.com>
2021-01-12 16:00:38 +0300

    Settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -8,163 +8,135 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  var name;
-  var surname;
 
-  Future<void> getData() async {
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser.uid)
-        .snapshots()
-        .listen((event) {
-      setState(() {
-        name = event.get("name");
-        surname = event.get("surname");
-        print(name);
-      });
-    });
-  }
 
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        height: MediaQuery.of(context).size.height / 1.4,
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: ListView(children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text("SETTINGS",
-                      style: TextStyle(fontSize: 40, color: Colors.grey[800])),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.all(3.0),
-                  padding: EdgeInsets.all(15.0),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: Row(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(FirebaseAuth.instance.currentUser.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return new CircularProgressIndicator();
+            }
+            var document = snapshot.data;
+            return new Container(
+              height: MediaQuery.of(context).size.height / 1.4,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: ListView(children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text(
-                        "Name:",
+                      Center(
+                        child: Text("SETTINGS",
+                            style: TextStyle(
+                                fontSize: 40, color: Colors.grey[800])),
+                      ),
+                      SizedBox(
+                        height: 10,
                       ),
                       Container(
-                          width: MediaQuery.of(context).size.height / 3.6,
-                          child: TextField())
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.all(3.0),
-                  padding: EdgeInsets.all(15.0),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Surname: ",
+                        margin: EdgeInsets.all(3.0),
+                        padding: EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Name:",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Container(
+                                width: MediaQuery.of(context).size.height / 3.6,
+                                child: TextField(
+                                  style: TextStyle(fontSize: 20),
+                                  decoration: InputDecoration(
+                                    hintText: document["name"],
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
                       ),
                       Container(
-                          width: MediaQuery.of(context).size.height / 3.6,
-                          child: TextField())
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.all(3.0),
-                  padding: EdgeInsets.all(15.0),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Birthday:",
+                        margin: EdgeInsets.all(3.0),
+                        padding: EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Surname:",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Container(
+                                width: MediaQuery.of(context).size.height / 3.6,
+                                child: TextField(
+                                  style: TextStyle(fontSize: 20),
+                                  decoration: InputDecoration(
+                                    hintText: document["surname"],
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
                       ),
                       Container(
-                          width: MediaQuery.of(context).size.height / 3.6,
-                          child: TextField())
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.all(3.0),
-                  padding: EdgeInsets.all(15.0),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: Row(
-                    children: [
-                      Text(
-                        "E-Mail: ",
+                        margin: EdgeInsets.all(3.0),
+                        padding: EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Birthday:",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Container(
+                                width: MediaQuery.of(context).size.height / 3.6,
+                                child: TextField(
+                                  style: TextStyle(fontSize: 20),
+                                  decoration: InputDecoration(
+                                    hintText: document["birthday"],
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
                       ),
                       Container(
-                          width: MediaQuery.of(context).size.height / 3.6,
-                          child: TextField())
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.all(3.0),
-                  padding: EdgeInsets.all(15.0),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Change Avatar",
+                        margin: EdgeInsets.all(3.0),
+                        padding: EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Change Avatar",style:TextStyle(fontSize: 20)
+                            ),
+                            
+                          ],
+                        ),
                       ),
-                      Container(
-                          width: MediaQuery.of(context).size.height / 3.6,
-                          child: TextField())
                     ],
                   ),
-                ),
-              ],
-            ),
-          ]),
-        ),
-      ),
+                ]),
+              ),
+            );
+          }),
     );
   }
-
-  _getName() {
-    return StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection("users")
-            .doc(FirebaseAuth.instance.currentUser.uid)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return new CircularProgressIndicator();
-          }
-          var document = snapshot.data;
-          return new Text(document["name"],
-              style: TextStyle(fontSize: 30));
-        });
-  }
-
 }
